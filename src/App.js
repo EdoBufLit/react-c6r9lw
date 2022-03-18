831e8ae7cfdc96ce1ab3217ec13edbf04d2012df
EdoBufLit <edoardo.buffa@outlook.it>
2022-03-18 23:07:37 +0100

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import StoreIcon from '@mui/icons-material/Store';
+import SearchIcon from '@mui/icons-material/Search';
+import Header from './Header.jsx'
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <Header />
     </div>
   );
 }
