589f9d5f53651340b61f18d73e1347759ca5742d
EdoBufLit <edoardo.buffa@outlook.it>
2022-03-19 13:26:40 +0100

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./style.css";
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import StoreIcon from '@mui/icons-material/Store';
-import SearchIcon from '@mui/icons-material/Search';
 import Header from './Header.jsx'
+import Home from './Home.jsx';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Home.css"
+
+function Home() {
+  return (
+    <div>
+      <div className="home">
+        <div className="home_container">
+          <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.doofinder.com%2Fit%2Fblog%2Fdefinizione-ecommerce&psig=AOvVaw2ThIFlLt7cxr_8GYwcy4Yl&ust=1647779088815000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCICd1tOV0vYCFQAAAAAdAAAAABAJ" alt="" className="home_image" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
